c22dbefbab0d87c1700c700a1e3c0855e4562ad6
Dustin Yulo <dustin.yulo@gmail.com>
2023-01-18 18:12:31 +0800

    starter files

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package DY_Tech_Portfolio
+ */
+
+?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ template-parts/header/site-header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Displays the site header.
+ *
+ * @package DY_Tech_Portfolio
+ */
+
+$wrapper_classes  = 'site-header';
+$wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
+$wrapper_classes .= ( true === get_theme_mod( 'display_title_and_tagline', true ) ) ? ' has-title-and-tagline' : '';
+$wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
+?>
+
+<header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>">
+
+	<h1>header</h1>
+
+</header><!-- #masthead -->
