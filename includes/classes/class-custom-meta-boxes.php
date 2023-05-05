55add813e2e38f286e9575f82f25733d85f9a9da
Dustin Yulo <dustin.yulo@gmail.com>
2023-05-06 02:40:52 +0800

    more project detail options

--- includes/classes/class-custom-meta-boxes.php
+++ includes/classes/class-custom-meta-boxes.php
@@ -46,11 +46,17 @@ class Custom_Meta_Boxes {
         wp_nonce_field( 'dy_tech_portfolio_custom_meta_box', 'dy_tech_portfolio_custom_meta_box_nonce' );
  
         if ( $post->post_type == 'dy_tp_project' ) {
-            $dy_tp_project_type = get_post_meta( $post->ID, '_dy_tech_portfolio_custom_meta_key_project_type', true );
-            $dy_tp_project_link = get_post_meta( $post->ID, '_dy_tech_portfolio_custom_meta_key_project_link', true );
-            $dy_tp_project_link_new_tab = get_post_meta( $post->ID, '_dy_tech_portfolio_custom_meta_key_project_link_new_tab', true ) == '1' ? 'checked' : '';
-            $dy_tp_project_link_rel_nofollow = get_post_meta( $post->ID, '_dy_tech_portfolio_custom_meta_key_project_link_rel_nofollow', true ) == '1' ? 'checked' : '';
-            $dy_tp_project_link_rel_noreferrer = get_post_meta( $post->ID, '_dy_tech_portfolio_custom_meta_key_project_link_rel_noreferrer', true ) == '1' ? 'checked' : '';
+            $dy_tp_project_post_meta = get_post_meta( $post->ID );
+            $dy_tp_project_type = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_type' ][ 0 ] ) ? $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_type' ][ 0 ] : '';
+            $dy_tp_project_short_description = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_short_description' ][ 0 ] ) ? $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_short_description' ][ 0 ] : '';
+            $dy_tp_project_link = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link' ][ 0 ] ) ? $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link' ][ 0 ] : '';
+            $dy_tp_project_link_new_tab = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link_new_tab' ][ 0 ] ) && $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link_new_tab' ][ 0 ] == '1' ? 'checked' : '';
+            $dy_tp_project_link_rel_nofollow = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link_rel_nofollow' ][ 0 ] ) && $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link_rel_nofollow' ][ 0 ] == '1' ? 'checked' : '';
+            $dy_tp_project_link_rel_noreferrer = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link_rel_noreferrer' ][ 0 ] ) && $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_link_rel_noreferrer' ][ 0 ] == '1' ? 'checked' : '';
+            $dy_tp_project_status = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_status' ][ 0 ] ) && $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_status' ][ 0 ] == 'demo' ? 'demo' : 'live';
+            $dy_tp_project_button_text = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_button_text' ][ 0 ] ) ? $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_button_text' ][ 0 ] : '';
+            $dy_tp_project_button_text_placeholder = $dy_tp_project_status == 'demo' ? __( 'View demo', 'dy-tech-portfolio' ) : __( 'Visit website', 'dy-tech-portfolio' );
+            $dy_tp_project_button_text_customize = isset( $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_button_text_customize' ][ 0 ] ) && $dy_tp_project_post_meta[ '_dy_tech_portfolio_custom_meta_key_project_button_text_customize' ][ 0 ] == '1' ? 'checked' : '';
         ?>
             <style>
                 .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-header {
@@ -59,14 +65,31 @@ class Custom_Meta_Boxes {
                 }
 
                 .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="text"],
-                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="url"] {
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="url"],
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data textarea {
                     width: 100%;
+                }
+
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data #dy_tech_portfolio_custom_meta_box_project_button_text {
+                    max-width: 200px;
+                }
+
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data .dy-tech-portfolio-custom-meta-box__table-data__project-link-options {
+                    margin-top: 0.25rem;
+                }
+
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="checkbox"],
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="radio"] {
+                    margin-top: 0.25rem;
                     margin-bottom: 0.25rem;
                 }
 
-                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="checkbox"] {
-                    margin-top: 0.25rem;
-                    margin-bottom: 0.25rem;
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data #dy_tech_portfolio_custom_meta_box_project_button_text_customize {
+                    margin-left: 0.5rem;
+                }
+
+                .dy-tech-portfolio-custom-meta-box__table .dy-tech-portfolio-custom-meta-box__table-data input[type="radio"]:not(:first-child) {
+                    margin-left: 1rem;
                 }
             </style>
             <table class='dy-tech-portfolio-custom-meta-box__table form-table'>
@@ -80,22 +103,74 @@ class Custom_Meta_Boxes {
                 </tr>
                 <tr>
                     <th class='dy-tech-portfolio-custom-meta-box__table-header' scope='row'>
+                        <label for='dy_tech_portfolio_custom_meta_box_project_short_description'><?php _e( 'Project short description', 'dy-tech-portfolio' ) ?></label>
+                    </th>
+                    <td class='dy-tech-portfolio-custom-meta-box__table-data'>
+                        <textarea id='dy_tech_portfolio_custom_meta_box_project_short_description' name='dy_tech_portfolio_custom_meta_box_project_short_description' rows='3'><?php echo esc_textarea( $dy_tp_project_short_description ); ?></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <th class='dy-tech-portfolio-custom-meta-box__table-header' scope='row'>
                         <label for='dy_tech_portfolio_custom_meta_box_project_link'><?php _e( 'Project link (URL)', 'dy-tech-portfolio' ) ?></label>
                     </th>
                     <td class='dy-tech-portfolio-custom-meta-box__table-data'>
                         <input type='url' id='dy_tech_portfolio_custom_meta_box_project_link' name='dy_tech_portfolio_custom_meta_box_project_link' value='<?php echo esc_attr( $dy_tp_project_link ); ?>' />
-                        <br />
-                        <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_link_new_tab' name='dy_tech_portfolio_custom_meta_box_project_link_new_tab' value='1' <?php echo esc_attr( $dy_tp_project_link_new_tab ); ?> />
-                        <label for='dy_tech_portfolio_custom_meta_box_project_link_new_tab'><?php _e( 'Open link in a new tab', 'dy-tech-portfolio' ) ?></label>
-                        <br />
-                        <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_link_rel_nofollow' name='dy_tech_portfolio_custom_meta_box_project_link_rel_nofollow' value='1' <?php echo esc_attr( $dy_tp_project_link_rel_nofollow ); ?> />
-                        <label for='dy_tech_portfolio_custom_meta_box_project_link_rel_nofollow'><?php _e( 'Add <code>rel="nofollow"</code> to link', 'dy-tech-portfolio' ) ?></label>
-                        <br />
-                        <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_link_rel_noreferrer' name='dy_tech_portfolio_custom_meta_box_project_link_rel_noreferrer' value='1' <?php echo esc_attr( $dy_tp_project_link_rel_noreferrer ); ?> />
-                        <label for='dy_tech_portfolio_custom_meta_box_project_link_rel_noreferrer'><?php _e( 'Add <code>rel="noreferrer"</code> to link', 'dy-tech-portfolio' ) ?></label>
+                        <br>
+                        <div class='dy-tech-portfolio-custom-meta-box__table-data__project-link-options'>
+                            <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_link_new_tab' name='dy_tech_portfolio_custom_meta_box_project_link_new_tab' value='1' <?php echo esc_attr( $dy_tp_project_link_new_tab ); ?> />
+                            <label for='dy_tech_portfolio_custom_meta_box_project_link_new_tab'><?php _e( 'Open link in a new tab', 'dy-tech-portfolio' ) ?></label>
+                            <br>
+                            <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_link_rel_nofollow' name='dy_tech_portfolio_custom_meta_box_project_link_rel_nofollow' value='1' <?php echo esc_attr( $dy_tp_project_link_rel_nofollow ); ?> />
+                            <label for='dy_tech_portfolio_custom_meta_box_project_link_rel_nofollow'><?php _e( 'Add <code>rel="nofollow"</code> to link', 'dy-tech-portfolio' ) ?></label>
+                            <br>
+                            <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_link_rel_noreferrer' name='dy_tech_portfolio_custom_meta_box_project_link_rel_noreferrer' value='1' <?php echo esc_attr( $dy_tp_project_link_rel_noreferrer ); ?> />
+                            <label for='dy_tech_portfolio_custom_meta_box_project_link_rel_noreferrer'><?php _e( 'Add <code>rel="noreferrer"</code> to link', 'dy-tech-portfolio' ) ?></label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th class='dy-tech-portfolio-custom-meta-box__table-header' scope='row'>
+                        <label><?php _e( 'Project status', 'dy-tech-portfolio' ) ?></label>
+                    </th>
+                    <td class='dy-tech-portfolio-custom-meta-box__table-data'>
+                        <input type='radio' id='dy_tech_portfolio_custom_meta_box_project_status_live' name='dy_tech_portfolio_custom_meta_box_project_status' value='live' <?php echo $dy_tp_project_status === 'live' ? esc_attr( 'checked' ) : ''; ?> />
+                        <label for='dy_tech_portfolio_custom_meta_box_project_status_live'><?php _e( 'Live', 'dy-tech-portfolio' ) ?></label>
+                        <input type='radio' id='dy_tech_portfolio_custom_meta_box_project_status_demo' name='dy_tech_portfolio_custom_meta_box_project_status' value='demo' <?php echo $dy_tp_project_status === 'demo' ? esc_attr( 'checked' ) : ''; ?> />
+                        <label for='dy_tech_portfolio_custom_meta_box_project_status_demo'><?php _e( 'Demo', 'dy-tech-portfolio' ) ?></label>
+                        <br>
+                        <p class='description'><?php _e( 'This simply alters the button appearance', 'dy-tech-portfolio' ) ?></p>
+                    </td>
+                </tr>
+                <tr>
+                    <th class='dy-tech-portfolio-custom-meta-box__table-header' scope='row'>
+                        <label for='dy_tech_portfolio_custom_meta_box_project_button_text'><?php _e( 'Button text', 'dy-tech-portfolio' ) ?></label>
+                    </th>
+                    <td class='dy-tech-portfolio-custom-meta-box__table-data'>
+                        <input type='text' id='dy_tech_portfolio_custom_meta_box_project_button_text' name='dy_tech_portfolio_custom_meta_box_project_button_text' placeholder='<?php echo esc_attr( $dy_tp_project_button_text_placeholder ); ?>' value='<?php echo esc_attr( $dy_tp_project_button_text ); ?>' <?php echo $dy_tp_project_button_text_customize !== 'checked' ? 'disabled' : ''; ?> />
+                        <input type='checkbox' id='dy_tech_portfolio_custom_meta_box_project_button_text_customize' name='dy_tech_portfolio_custom_meta_box_project_button_text_customize' value='1' onchange='customizeProjectButtonText()' <?php echo esc_attr( $dy_tp_project_button_text_customize ); ?> />
+                        <label for='dy_tech_portfolio_custom_meta_box_project_button_text_customize'><?php _e( 'Customize button text?', 'dy-tech-portfolio' ) ?></label>
                     </td>
                 </tr>
             </table>
+            <script>
+                const projectStatusLive = document.getElementById( 'dy_tech_portfolio_custom_meta_box_project_status_live' );
+                const projectStatusDemo = document.getElementById( 'dy_tech_portfolio_custom_meta_box_project_status_demo' );
+                const projectButtonText = document.getElementById( 'dy_tech_portfolio_custom_meta_box_project_button_text' );
+                const projectButtonTextCustomize = document.getElementById( 'dy_tech_portfolio_custom_meta_box_project_button_text_customize' );
+
+                projectStatusLive.addEventListener( 'click', function() {
+                    projectButtonText.placeholder = '<?php echo esc_attr__( 'Visit website', 'dy-tech-portfolio' ); ?>';
+                } );
+
+                projectStatusDemo.addEventListener( 'click', function() {
+                    projectButtonText.placeholder = '<?php echo esc_attr__( 'View demo', 'dy-tech-portfolio' ); ?>';
+                });
+
+                function customizeProjectButtonText() {
+                    projectButtonText.disabled = !projectButtonTextCustomize.checked;
+                    projectButtonText.value = !projectButtonTextCustomize.checked ? '' : projectButtonText.value;
+                }
+            </script>
         <?php 
         }
     }
@@ -139,6 +214,12 @@ class Custom_Meta_Boxes {
                 delete_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_type' );
             }
 
+            if ( isset( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_short_description' ] ) && ! empty( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_short_description' ] ) ) {
+                update_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_short_description', sanitize_textarea_field( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_short_description' ] ) );
+            } else {
+                delete_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_short_description' );
+            }
+
             if ( isset( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_link' ] ) && ! empty( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_link' ] ) ) {
                 update_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_link', esc_url_raw( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_link' ] ) );
             } else {
@@ -161,6 +242,24 @@ class Custom_Meta_Boxes {
                 update_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_link_rel_noreferrer', '1' );
             } else {
                 delete_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_link_rel_noreferrer' );
+            }
+
+            if ( isset( $_POST['dy_tech_portfolio_custom_meta_box_project_status'] ) && ! empty( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_status' ] ) ) {
+                update_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_status', sanitize_key( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_status' ] ) );
+            } else {
+                delete_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_status' );
+            }
+
+            if ( isset( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_button_text' ] ) && ! empty( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_button_text' ] ) ) {
+                update_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_button_text', sanitize_text_field( $_POST[ 'dy_tech_portfolio_custom_meta_box_project_button_text' ] ) );
+            } else {
+                delete_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_button_text' );
+            }
+
+            if ( isset( $_POST['dy_tech_portfolio_custom_meta_box_project_button_text_customize'] ) && $_POST['dy_tech_portfolio_custom_meta_box_project_button_text_customize'] == '1' ) {
+                update_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_button_text_customize', '1' );
+            } else {
+                delete_post_meta( $post_id, '_dy_tech_portfolio_custom_meta_key_project_button_text_customize' );
             }
         }
     }
