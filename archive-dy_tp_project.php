47530640a7c85eb05c958a07d0e16eec6a33da7f
Dustin Yulo <dustin.yulo@gmail.com>
2024-03-13 05:12:27 +0800

    projects archive page and new wp admin bar height

---
+++ archive-dy_tp_project.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the projects list
+ *
+ * @package DY_Tech_Portfolio
+ */
+
+get_header();
+
+$description = get_the_archive_description();
+?>
+
+<?php if ( have_posts() ) : ?>
+
+	<header class="page-header">
+		<h1 class="page-title"><?php _e( 'My projects', 'dy-tech-portfolio' ) ?></h1>
+		<?php if ( $description ) : ?>
+			<h2 class="archive-description"><?php echo wp_strip_all_tags( $description ); ?></h2>
+		<?php endif; ?>
+	</header>
+
+	<div class="projects-container">
+		<?php while ( have_posts() ) : ?>
+			<?php the_post(); ?>
+			<?php get_template_part( 'template-parts/content/content', 'archive-project' ); ?>
+		<?php endwhile; ?>
+	</div>
+
+<?php else : ?>
+	<?php //get_template_part( 'template-parts/content/content-none' ); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
---
+++ template-parts/content/content-archive-project.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying projets grid
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DY_Tech_Portfolio
+ */
+
+ $dy_tp_project_post_meta = get_post_meta( get_the_ID() );
+ $dy_tp_project_type = isset( $dy_tp_project_post_meta[ '_dy_tp_project_type' ][ 0 ] ) ? $dy_tp_project_post_meta[ '_dy_tp_project_type' ][ 0 ] : '';
+ $dy_tp_project_snapshot = isset( $dy_tp_project_post_meta[ '_dy_tp_project_snapshot' ][ 0 ] ) ? $dy_tp_project_post_meta[ '_dy_tp_project_snapshot' ][ 0 ] : '';
+ $dy_tp_project_snapshot_alt_text = !empty( $dy_tp_project_snapshot ) ? get_post_meta( attachment_url_to_postid( $dy_tp_project_snapshot ), '_wp_attachment_image_alt', true ) : '';
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<a href="<?php echo esc_url( get_permalink() ); ?>">
+			<?php if( $dy_tp_project_snapshot ) { ?>
+				<div class="project-snapshot">
+					<img src="<?php echo esc_url( $dy_tp_project_snapshot ); ?>" alt="<?php echo esc_attr( $dy_tp_project_snapshot_alt_text ); ?>" />
+				</div>
+			<?php } else if( has_post_thumbnail() ) { ?>
+				<div class="project-snapshot">
+					<?php the_post_thumbnail( 'full' ); ?>
+				</div>
+			<?php } ?>
+			<p class="project-type"><?php echo esc_html( $dy_tp_project_type ); ?></p>
+			<?php the_title( '<h3 class="project-title">', '</h3>' ); ?>
+		</a>
+	</header>
+</article>
