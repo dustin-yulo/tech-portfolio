fa04e0e2e2a0562114b5f3c621888a53721d3991
Dustin Yulo <dustin.yulo@gmail.com>
2024-03-13 05:12:47 +0800

    empty post title

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -13,7 +13,14 @@
 	<header class="entry-header">
 		<a href="<?php echo esc_url( get_permalink() ); ?>">
 			<?php if( has_post_thumbnail() ) { the_post_thumbnail( 'full' ); } ?>
-			<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+
+			<?php if( !empty( get_the_title() ) ) { ?>
+				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+			<?php } else { ?>
+				<h2 class="entry-title">
+					<?php esc_html_e( '(no title)', 'dy-tech-portfolio' ) ?>
+				</h2>
+			<?php } ?>
 		</a>
 	</header>
 
