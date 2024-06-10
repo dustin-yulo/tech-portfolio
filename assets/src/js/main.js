6f0a5494bac79451c647c0c42a64983d4b9bd87c
Dustin Yulo <dustin.yulo@gmail.com>
2024-06-11 07:11:51 +0800

    cleanup for production use

--- assets/src/js/main.js
+++
@@ -1,8 +0,0 @@
-jQuery( document ).ready( function( $ ) {
-
-    // Mobile menu toggle
-    $( '#mobile-header .menu-toggle' ).on( 'click', function() {
-        $( '#mobile-header #mobile-topbar .menu-toggle, #mobile-header #mobile-topbar-dropdown' ).toggleClass( 'active' );
-    } );
-
-} );
